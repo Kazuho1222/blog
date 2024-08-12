a5d6df47f48bbccff0143396b66664c49f8a90d0
Kazuho1222 <destiny5914@gmail.com>
2024-08-12 22:27:12 +0900

    ３つのコンポーネントで2段組みのレイアウトを構成する

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import Hero from '../components/hero'
+import Contact from '../components/contact'
 import Container from '../components/container'
+import Hero from '../components/hero'
+import PostBody from '../components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from '../components/two-column'
 
 export default function About() {
   return (
@@ -9,20 +11,30 @@ export default function About() {
         title='About'
         subtitle='About development activities'
         imageOn={false} />
-      <p>
-        Cubeが得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びづけるクリエイティブを提案し続けています。
-      </p>
-      <h2>モノづくりで目指していること</h2>
-      <p>
-        モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力をいれています。
-      </p>
-      <p>
-        単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
-      </p>
-      <h3>新しいことへのチャレンジ</h3>
-      <p>
-        今までと違くものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
-      </p>
+      <TwoColumn>
+        <TwoColumnMain>
+          <PostBody>
+            <p>
+              Cubeが得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びづけるクリエイティブを提案し続けています。
+            </p>
+            <h2>モノづくりで目指していること</h2>
+            <p>
+              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力をいれています。
+            </p>
+            <p>
+              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
+            </p>
+            <h3>新しいことへのチャレンジ</h3>
+            <p>
+              今までと違くものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
+            </p>
+          </PostBody>
+        </TwoColumnMain>
+
+        <TwoColumnSidebar>
+          <Contact />
+        </TwoColumnSidebar>
+      </TwoColumn>
     </Container>
   )
 }
