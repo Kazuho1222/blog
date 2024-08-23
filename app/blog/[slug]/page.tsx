38d04464d21b0166964a45743f7ddbf7d0e58a57
Kazuho1222 <destiny5914@gmail.com>
2024-08-24 00:32:49 +0900

    ページネーションを追加する

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/app/components/container";
 import ConvertBody from "@/app/components/convert-body";
+import pagination from "@/app/components/pagination";
 import PostBody from "@/app/components/post-body";
 import PostCategories from "@/app/components/post-categories";
 import PostHeader from "@/app/components/post-header";
@@ -8,54 +9,68 @@ import { getAllSlugs, getPostBySlug } from "@/app/lib/api";
 import { eyecatchLocal, siteMeta } from "@/app/lib/constants";
 import extractText from "@/app/lib/extract-text";
 import { getImageBuffer } from "@/app/lib/getImageBuffer";
+import { prevNextPost } from "@/app/lib/prev-next-post";
 import Image from "next/legacy/image";
 import { getPlaiceholder } from "plaiceholder";
+import Pagination from "@/app/components/pagination";
 const { siteTitle, siteUrl } = siteMeta
 
 export default async function Post({ params }: { params: { slug: any } }) {
   const slug = params.slug
   const post = await getPostBySlug(slug)
-  const { title, publishData: publish, content, categories } = post
-  const description = extractText(content)
-  const eyecatch = post.eyecatch ?? eyecatchLocal
-  const imageBuffer = await getImageBuffer(eyecatch.url)
-  const { base64 } = await getPlaiceholder(imageBuffer)
-  eyecatch.blurDataURL = base64
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const { title, publishData: publish, content, categories } = post
+    const description = extractText(content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+    const imageBuffer = await getImageBuffer(eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    eyecatch.blurDataURL = base64
 
-  return (
-    <Container large={false}>
-      <article>
-        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
-        <figure>
-          <Image
-            key={eyecatch.url}
-            src={eyecatch.url}
-            alt=""
-            layout="responsive"
-            width={eyecatch.width}
-            height={eyecatch.height}
-            sizes="(min-width:1152px)1152px,100vw"
-            priority
-            placeholder="blur"
-            blurDataURL={eyecatch.blurDataURL}
+    return (
+      <Container large={false}>
+        <article>
+          <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+
+          <figure>
+            <Image
+              key={eyecatch.url}
+              src={eyecatch.url}
+              alt=""
+              layout="responsive"
+              width={eyecatch.width}
+              height={eyecatch.height}
+              sizes="(min-width:1152px)1152px,100vw"
+              priority
+              placeholder="blur"
+              blurDataURL={eyecatch.blurDataURL}
+            />
+          </figure>
+
+          <TwoColumn>
+            <TwoColumnMain>
+              <PostBody>
+                <ConvertBody contentHTML={content} />
+              </PostBody>
+            </TwoColumnMain>
+            <TwoColumnSidebar>
+              <PostCategories categories={categories} />
+            </TwoColumnSidebar>
+          </TwoColumn>
+          <Pagination
+            prevText={prevPost.title}
+            prevUrl={`/blog/${prevPost.slug}`}
+            nextText={nextPost.title}
+            nextUrl={`/blog/${nextPost.slug}`}
           />
-        </figure>
-
-        <TwoColumn>
-          <TwoColumnMain>
-            <PostBody>
-              <ConvertBody contentHTML={content} />
-            </PostBody>
-          </TwoColumnMain>
-          <TwoColumnSidebar>
-            <PostCategories categories={categories} />
-          </TwoColumnSidebar>
-        </TwoColumn>
-
-      </article>
-    </Container>
-  )
+        </article>
+      </Container>
+    )
+  }
 }
 
 export const dynamicParams = false
---
+++ app/components/pagination.tsx
@@ -0,0 +1,36 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import styles from '../../styles/pagination.module.css'
+
+export default function pagination({
+  prevText = '',
+  prevUrl = '',
+  nextText = '',
+  nextUrl = '',
+}) {
+  return (
+    <ul className={styles.flexContainer}>
+      {prevText && prevUrl && (
+        <li className={styles.prev}>
+          <Link href={prevUrl}>
+            <div className={styles.iconText} >
+              <FontAwesomeIcon icon={faChevronLeft} color='var(--gray-25)' />
+              <span>{prevText}</span>
+            </div>
+          </Link>
+        </li>
+      )}
+      {nextText && nextUrl && (
+        <li className={styles.next}>
+          <Link href={nextUrl}>
+            <div className={styles.iconText}>
+              <span>{nextText}</span>
+              <FontAwesomeIcon icon={faChevronRight} color='var(--gray-25)' />
+            </div>
+          </Link>
+        </li>
+      )}
+    </ul>
+  )
+}
