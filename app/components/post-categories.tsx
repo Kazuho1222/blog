ee44e3c60dea8be7effec14069921b7131697f63
Kazuho1222 <destiny5914@gmail.com>
2024-11-12 00:42:53 +0900

    css変更

--- app/components/post-categories.tsx
+++ app/components/post-categories.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../../styles/post-catefories.module.css';
+import styles from '../../styles/post-categories.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
 
