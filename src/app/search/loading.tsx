60f5705ed3e50121b32a5c7ead83844052d509d4
Kazuho1222 <destiny5914@gmail.com>
2026-03-22 15:41:41 +0900

    fix: Use nanoid for key and _i for unused param in src/app/search/loading.tsx

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { Skeleton } from '@/components/ui/skeleton'
 import Container from '@/src/components/container'
 
@@ -10,8 +11,8 @@ export default function Loading() {
 
         {/* カード一覧 */}
         <div className="grid grid-cols-1 gap-6 mt-8 sm:grid-cols-2 lg:grid-cols-3">
-          {Array.from({ length: 9 }).map((_, i) => (
-            <div key={`search-skeleton-${i}`} className="space-y-3">
+          {Array.from({ length: 9 }).map((_, _i) => (
+            <div key={nanoid()} className="space-y-3">
               {/* 画像 */}
               <Skeleton className="h-40 w-full rounded-lg" />
 
