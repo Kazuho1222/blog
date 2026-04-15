08c9059ca8a1d3202d7a526967fb0ae7758fa3c1
Kazuho1222 <destiny5914@gmail.com>
2026-04-15 14:53:07 +0900

    fix

--- src/components/extensions/link-card.ts
+++ src/components/extensions/link-card.ts
@@ -1,4 +1,4 @@
-import { Node, mergeAttributes, nodeInputRule } from '@tiptap/core'
+import { mergeAttributes, Node, nodeInputRule } from '@tiptap/core'
 import { ReactNodeViewRenderer } from '@tiptap/react'
 import LinkCardView from '../link-card-view'
 
