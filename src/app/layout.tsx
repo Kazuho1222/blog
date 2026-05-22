c719dc27c4914faa98aab43678cc8d58720dad71
Kazuho1222 <destiny5914@gmail.com>
2026-05-22 22:25:30 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>
           <Suspense fallback={null}>
             <GoogleAnalytics />
           </Suspense>
@@ -45,8 +45,8 @@ export default function RootLayout({
           {children}
           <Toaster />
           <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
